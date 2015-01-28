4bac010a21433fc0bdd1434a151d3819ed1e7f73
vhphuc <pirent420@gmail.com>
2015-01-28 17:47:43 +0700

    Add more

--- hf_sql.sql
+++ hf_sql.sql
@@ -14,6 +14,35 @@ CREATE TABLE my_contacts
 	profession VARCHAR(50),
 	location VARCHAR(50),
 	status VARCHAR(20),
-	interest VARCHAR(100),
+	interests VARCHAR(100),
 	seeking VARCHAR(100)
 );
+
+DROP TABLE my_contacts;
+
+CREATE TABLE my_contacts
+(
+	last_name VARCHAR(30),
+	first_name VARCHAR(20),
+	email VARCHAR(50),
+	gender CHAR(1),
+	birthday DATE,
+	profession VARCHAR(50),
+	location VARCHAR(50),
+	status VARCHAR(20),
+	interests VARCHAR(100),
+	seeking VARCHAR(100)
+);
+
+INSERT INTO my_contacts
+(last_name, first_name, email, gender, birthday, profession,
+location, status, interests, seeking)
+VALUES
+('Anderson', 'Jillian', 'jill_anderson@breakneckpizza.com', 'F', 
+'1980-09-05', 'Technical Writer', 'Palo Alto, CA', 'Single', 
+'Kayaking, Reptiles', 'Relationship, Friends');
+
+INSERT INTO my_contacts 
+(first_name, email, profession, location)
+VALUES
+('Pat', 'patpost@breakneckpizza.com', 'Postal Worker', 'Princeton, NJ');
